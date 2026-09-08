aab0aa0933376f55b3248e869292c78b23180b0e
ilyabaykalov <ibaykalov@blogic.ru>
2026-09-08 17:35:14 +0300

    Создан каркас сервиса corelia-document-service

---
+++ src/main/java/ru/corelia/documents/DocumentApplication.java
@@ -0,0 +1,27 @@
+package ru.corelia.documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ru.corelia.config.LocalEnvironment;
+
+/** Запускает приложение corelia-document-service. */
+@SpringBootApplication(
+        scanBasePackages = {
+            "ru.corelia.config",
+            "ru.corelia.profile",
+            "ru.corelia.support",
+            "ru.corelia.auth",
+            "ru.corelia.http",
+            "ru.corelia.cache",
+            "ru.corelia.integration",
+            "ru.corelia.transport",
+            "ru.corelia.documents"
+        })
+public class DocumentApplication {
+    public static void main(String[] args) {
+        var app = new SpringApplication(DocumentApplication.class);
+        app.setDefaultProperties(LocalEnvironment.load());
+        app.run(args);
+    }
+}
